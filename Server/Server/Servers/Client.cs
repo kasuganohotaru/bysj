bd92c8481e8850f78fc78694f8ffdc60985b90ba
春日野荧 <Moying2063@gmail.com>
2022-11-07 08:44:12 +0800

    添加googleProtobuf

---
+++ Server/Server/Servers/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using GameServer.Tool;
+
+namespace GameServer.Servers
+{
+    class Client
+    {
+        private Socket ClientSocket;
+        private Server server;
+        private Message message;
+
+
+        public Client(Socket socket,Server _server)
+        {
+            ClientSocket = socket;
+            server = _server;
+        }
+
+        /// <summary>
+        /// 异步接收消息
+        /// </summary>
+        private void StartReceive()
+        {
+            ClientSocket.BeginReceive(message.Buffer, message.StartIndex, message.Remsize, SocketFlags.None, ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult iar)
+        {
+
+        }
+    }
+
+}
---
+++ Server/Server/Servers/Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace GameServer.Servers
+{
+    class Server
+    {
+        private Socket ServerSocket;
+        private List<Client> clients = new List<Client>();
+
+        public Server(int port)
+        {
+            ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ServerSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+            ServerSocket.Listen(0);
+            StartAccept();
+        }
+
+        /// <summary>
+        /// 异步连接客户端
+        /// </summary>
+        private void StartAccept()
+        {
+            ServerSocket.BeginAccept(AcceptCallBack,null);
+        }
+        
+        private void AcceptCallBack(IAsyncResult asyncResult)
+        {
+            //将连接到的客户端添加到用户列表中
+            Socket ClientSocket = ServerSocket.EndAccept(asyncResult);
+            clients.Add(new Client(ClientSocket,this));
+            StartAccept();
+        }
+    }
+}
