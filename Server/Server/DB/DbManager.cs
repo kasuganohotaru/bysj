9904fd4e08ce41f76b67f60399a6373bad06c5a4
春日野荧 <Moying2063@gmail.com>
2022-11-15 15:00:37 +0800

    添加数据库相关代码

---
+++ Server/Server/DB/DbManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace GameServer.DB
+{
+    class DbManager:SingletonBase<DbManager>
+    {
+        public string _constr = "";
+
+        public override void Init()
+        {
+            string temp = ConfigurationManager.ConnectionStrings["PixelWarDB"].ConnectionString;
+            _constr = temp;
+            return;
+        }
+
+        private SqlConnection OpenDB()//打开数据库，返回连接
+        {
+            Init();
+            SqlConnection connection = new SqlConnection(_constr);
+            try
+            {
+                connection.Open();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("打开数据库出错:" + e);
+            }
+            return connection;
+        }
+    }
+
+}
---
+++ Server/Server/Servers/SingletonBase.cs
@@ -0,0 +1,28 @@
+﻿//线程安全的单例基类
+public abstract class SingletonBase<T> where T : class, new()
+{
+    private static T _instance;
+    private static readonly object syslock = new object();
+
+    protected SingletonBase()
+    {
+
+    }
+
+    public abstract void Init();
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (syslock)
+                {
+                    if (_instance == null)
+                        _instance = new T();
+                }
+            }
+            return _instance;
+        }
+    }
+}
